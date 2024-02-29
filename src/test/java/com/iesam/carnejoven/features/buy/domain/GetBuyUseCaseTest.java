bde2a1026291478bb6cbda75a828bd321c46769b
Juan Camilo Zuñiga Zuñiga <juanc.zunzun@educa.jcyl.es>
2024-02-29 16:49:39 +0200

    Create new test for buy

--- src/test/java/com/iesam/carnejoven/features/buy/domain/GetBuyUseCaseTest.java
+++ src/test/java/com/iesam/carnejoven/features/buy/domain/GetBuyUseCaseTest.java
@@ -2,6 +2,8 @@ package com.iesam.carnejoven.features.buy.domain;
 
 import com.iesam.carnejoven.features.buy.data.Stub1BuyDataRepository;
 import com.iesam.carnejoven.features.buy.data.Stub2BuyDataRepository;
+import com.iesam.carnejoven.features.promotions.domain.Promotions;
+import com.iesam.carnejoven.features.user.domain.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GetBuyUseCaseTest {
     private GetBuyUseCase getBuyUseCase;
+
     @BeforeEach
     void setUp() {
 
@@ -24,31 +27,54 @@ class GetBuyUseCaseTest {
     @Test
     void execute() {
     }
+
     @Test
     public void CuandoBuySeaNuloMostrarNull() {
         //Given
         getBuyUseCase = new GetBuyUseCase(
                 new Stub1BuyDataRepository()
         );
-        String id="1";
+        String id = "1";
         //When
-        Buy buy=getBuyUseCase.execute(id);
+        Buy buy = getBuyUseCase.execute(id);
 
         //Then
         Assertions.assertNull(buy);
     }
 
-    public void CuandoBuyNoSeaNuloMuestreElObjeto(){
+    @Test
+    public void CuandoBuyNoSeaNuloMuestreElObjeto() {
         //Given
         getBuyUseCase = new GetBuyUseCase(
                 new Stub2BuyDataRepository()
         );
-        String id="1";
+        String id = "1";
         //When
-        Buy buyNotNull=getBuyUseCase.execute(id);
+        Buy buyNotNull = getBuyUseCase.execute(id);
 
         //Then
         Assertions.assertNotNull(buyNotNull);
+    }
+
+    @Test
+    public void CuandoBuyNoSeaNuloVerifiqueSiElObjetoEstaBien() {
+        //Given
+        getBuyUseCase = new GetBuyUseCase(
+                new Stub2BuyDataRepository()
+        );
+        String id = "1";
+        Buy buyExpected = new Buy("1", "a", new Promotions("1", "a", "a", "15"), new User("1", "a", "a", "a"));
+
+        //When
+        Buy result = getBuyUseCase.execute(id);
+
+        //then
+        Assertions.assertEquals(buyExpected.getId(), result.getId());
+        Assertions.assertEquals(buyExpected.getDateBuy(), result.getDateBuy());
+        Assertions.assertEquals(buyExpected.getPromotions(), result.getPromotions());
+        Assertions.assertEquals(buyExpected.getUser(),result.getUser());
+
+
     }
 
 }
---
+++ src/test/java/com/iesam/carnejoven/features/promotions/data/Stub1PromotionDataRepository.java
@@ -0,0 +1,4 @@
+package com.iesam.carnejoven.features.promotions.data;
+
+public class Stub1PromotionDataRepository {
+}
